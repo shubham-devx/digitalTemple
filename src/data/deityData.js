373d4eaaa52388bed1d9b76f1455e0e1f68deacd
shubham <subhamv2003@gmail.com>
2026-03-07 14:06:13 +0530

    v

--- src/data/deityData.js
+++ src/data/deityData.js
@@ -3,28 +3,28 @@ const deityData = [
 name: "Krishna",
 image: "/krishna.jpg",
 mantra: "Hare Krishna Hare Krishna Krishna Krishna Hare Hare Hare Rama Hare Rama Rama Rama Hare Hare",
-aarti: "/krishna.mp3"
+aarti: "/krishna.mpeg"
 },
 
 {
 name: "Shiva",
 image: "/shiva.jpg",
 mantra: "Om Namah Shivaya",
-aarti: "/shiva.mp3"
+aarti: "/shiva.mpeg"
 },
 
 {
 name: "Hanuman",
 image: "/hanuman.jpg",
 mantra: "Om Hanumate Namah",
-aarti: "/hanuman.mp3"
+aarti: "/hanuman.mpeg"
 },
 
 {
 name: "Ganesha",
 image: "/ganesha.jpg",
 mantra: "Om Gan Ganapataye Namah",
-aarti: "/ganesha.mp3"
+aarti: "/ganesha.mpeg"
 }
 ];
 
